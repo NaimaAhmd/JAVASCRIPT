334b958ecbe7b5558bc2621d3ebc17873c35d218
NaimaAhmd <na0418442@gmail.com>
2024-05-14 10:20:40 +0300

    js

---
+++ second.js
@@ -0,0 +1,36 @@
+ {
+    var x =40;
+    console.log(x)
+    x=40
+
+ }
+{
+  let x=10; 
+  console.log(x);
+  x=10
+}
+
+let y=50
+{
+   console.log(y)
+   y=50 
+
+   const t=60.6
+   console.log(t);
+}
+
+{
+    let l=30
+    let w=70
+    let area=l*w
+    console.log(area)
+}
+{
+    const pie=3.142
+    let diameter=20
+    let raduis=diameter/2
+    let area=pie*raduis*raduis
+    console.log(area)
+
+
+}
